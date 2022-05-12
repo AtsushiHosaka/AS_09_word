3d1c371091a131d844c83df4e278aaec0d728317
AtsushiHosaka <hosaka2917@gmail.com>
2022-05-12 17:52:33 +0900

    Initial Commit

---
+++ AS_09_word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AS_09_word
+//
+//  Created by 保坂篤志 on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
